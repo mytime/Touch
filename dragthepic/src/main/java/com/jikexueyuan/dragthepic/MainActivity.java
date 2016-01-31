39aa46b2f4aeb2be05ff590b8247a817564b80df
mytime <cmba@qq.com>
2016-01-31 22:07:47 +0800

    drag the pic

--- dragthepic/src/main/java/com/jikexueyuan/dragthepic/MainActivity.java
+++ dragthepic/src/main/java/com/jikexueyuan/dragthepic/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jikexueyuan.dragthepic;
 
+import android.app.ActionBar;
+import android.graphics.Rect;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,26 +19,36 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private View rootView;
     private int actionBarHeight;
+    private int notifiHeight;
+    private ImageView moveImageView;
+
+    private float diffX;
+    private float diffY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //
-        rootView = getLayoutInflater().inflate(R.layout.activity_main,null);
-        //发送一个新新线程请求
+        rootView = getLayoutInflater().inflate(R.layout.activity_main, null);
+        //发送一个新线程请求
         rootView.post(new Runnable() {
             @Override
             public void run() {
                 //ActionBar
-                if (getSupportActionBar() != null){
+                if (getSupportActionBar() != null) {
                     //获取actionBar高度
                     actionBarHeight = getSupportActionBar().getHeight();
-                    // TODO: 2016/1/31  
+
+                    //获取动态拦的高度
+                    Rect frame = new Rect();
+                    getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+                    notifiHeight = frame.top;
                 }
 
             }
-        })
+        });
 
         imageView = (ImageView) findViewById(R.id.imageView);
     }
@@ -48,26 +60,50 @@ public class MainActivity extends AppCompatActivity {
 
         //下标
         int pointIndex = MotionEventCompat.getActionIndex(event);
-        switch (actionTag){
+        switch (actionTag) {
             case MotionEvent.ACTION_DOWN:
+
+                //图片初始位置
+                RelativeLayout.LayoutParams downParams =
+                        (RelativeLayout.LayoutParams) imageView.getLayoutParams();
+
+                //点击区域在图片内
+                if (getTouchXPotion(event, pointIndex) > downParams.leftMargin
+                        && getTouchXPotion(event, pointIndex) < downParams.leftMargin + imageView.getWidth()
+                        && getTouchYPotion(event, pointIndex) - actionBarHeight - notifiHeight > downParams.topMargin
+                        && getTouchYPotion(event, pointIndex) - actionBarHeight - notifiHeight < downParams.topMargin + imageView.getHeight()) {
+
+                    moveImageView = imageView;
+
+                    //X轴差值
+                    diffX = getTouchXPotion(event,pointIndex) - downParams.leftMargin;
+                    //Y轴差值
+                    diffY = getTouchYPotion(event,pointIndex) - downParams.topMargin;
+                }
+
 
                 break;
             case MotionEvent.ACTION_MOVE:
 
-                //获取图片布局属性
-                RelativeLayout.LayoutParams param =
-                        (RelativeLayout.LayoutParams) imageView.getLayoutParams();
-                //获取X坐标
-                param.leftMargin = (int) getTouchXPotion(event,pointIndex);
-                //获取Y坐标
-                param.topMargin = (int) getTouchYPotion(event,pointIndex);
+                if (moveImageView != null) {
+                    //获取图片布局属性
+                    RelativeLayout.LayoutParams param = (RelativeLayout.LayoutParams) imageView.getLayoutParams();
+                    //获取X坐标
+                    param.leftMargin = (int) (getTouchXPotion(event, pointIndex) - diffX);
+                    //获取Y坐标
+                    param.topMargin = (int) (getTouchYPotion(event,
+                            pointIndex)-diffY - actionBarHeight-notifiHeight);
 
-                //设置属性
-                imageView.setLayoutParams(param);
+                    //设置属性
+                    imageView.setLayoutParams(param);
+                }
+
 
                 break;
             case MotionEvent.ACTION_UP:
 
+                //抬起时清空
+                moveImageView = null;
                 break;
         }
 
@@ -76,11 +112,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //触摸X地址
-    private float getTouchXPotion(MotionEvent event,int pointIndex){
+    private float getTouchXPotion(MotionEvent event, int pointIndex) {
         return MotionEventCompat.getX(event, pointIndex);
     }
+
     //触摸Y地址
-    private float getTouchYPotion(MotionEvent event,int pointIndex){
-        return MotionEventCompat.getY(event,pointIndex);
+    private float getTouchYPotion(MotionEvent event, int pointIndex) {
+        return MotionEventCompat.getY(event, pointIndex);
     }
 }
